52a9799f684cd61b3c4e3fc1260ee3bb39991870
sumit singh tomar <sumitsinght10@gmail.com>
2024-03-08 23:31:17 +0530

    Adding Rest Api in typescript

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const express_1 = __importDefault(require("express"));
+const todo_1 = __importDefault(require("./routes/todo"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use('/', todo_1.default);
+const server = http_1.default.createServer(app);
+server.listen(5000);
--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import http from 'http'
 import express from 'express'
 import todoRouter from './routes/todo'
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json())
 
-app.use('/',todoRouter)
+app.use('/', todoRouter)
 
 const server = http.createServer(app)
 
